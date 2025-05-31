874999fe3287774d9e52c7177bd725f5de0abdf9
Susana Rueda <susanalrueda@uts.edu.co>
2025-05-31 00:21:42 -0500

    Proyecto Completo

---
+++ js/productos.js
@@ -0,0 +1,26 @@
+// js/productos.js
+const productos = [
+  // Prendas Inferiores (6 productos)
+  { id: 1, categoria: "prendas-inferiores", nombre: "Pantalón Tiro Alto", precio: 50000, imagen: "images/products/pantalon_casual.png" },
+  { id: 2, categoria: "prendas-inferiores", nombre: "Jeans Bota Recta", precio: 60000, imagen: "images/products/pantalon2.png" },
+  { id: 3, categoria: "prendas-inferiores", nombre: "Short", precio: 35000, imagen: "images/products/short1.png" },
+  { id: 4, categoria: "prendas-inferiores", nombre: "Jean Wide Leg", precio: 70000, imagen: "images/products/pantalon3.png" },
+  { id: 5, categoria: "prendas-inferiores", nombre: "Falda Short", precio: 40000, imagen: "images/products/falda1.png" },
+  { id: 6, categoria: "prendas-inferiores", nombre: "Falda Larga", precio: 30000, imagen: "images/products/falda_larga.png" },
+
+  // Prendas Superiores (6 productos)
+  { id: 7, categoria: "prendas-superiores", nombre: "Body Manga Larga", precio: 40000, imagen: "images/products/body.png" },
+  { id: 8, categoria: "prendas-superiores", nombre: "Body", precio: 55000, imagen: "images/products/body2.png" },
+  { id: 9, categoria: "prendas-superiores", nombre: "Blusa Straple", precio: 25000, imagen: "images/products/blusa1.png" },
+  { id: 10, categoria: "prendas-superiores", nombre: "Blusa un Solo Hombro", precio: 60000, imagen: "images/products/blusah.png" },
+  { id: 11, categoria: "prendas-superiores", nombre: "Chaqueta Bomber", precio: 85000, imagen: "images/products/chaqueta1.png" },
+  { id: 12, categoria: "prendas-superiores", nombre: "Chaqueta Biker Cuero", precio: 30000, imagen: "images/products/chaqueta2.png" },
+
+  // Vestidos (6 productos)
+  { id: 13, categoria: "vestidos", nombre: "Vestido Corto Bohemio", precio: 90000, imagen: "images/products/vestido1.png" },
+  { id: 14, categoria: "vestidos", nombre: "Vestido Tejido con lurex", precio: 70000, imagen: "images/products/vestido2.png" },
+  { id: 15, categoria: "vestidos", nombre: "Vestido Tipo Polo", precio: 45000, imagen: "images/products/vestido3.png" },
+  { id: 16, categoria: "vestidos", nombre: "Vestido Camisero", precio: 65000, imagen: "images/products/vestido4.png" },
+  { id: 17, categoria: "vestidos", nombre: "Vestido Metalizado", precio: 85000, imagen: "images/products/vestido5.png" },
+  { id: 18, categoria: "vestidos", nombre: "Vestido Largo", precio: 100000, imagen: "images/products/vestido6.png" }
+];
